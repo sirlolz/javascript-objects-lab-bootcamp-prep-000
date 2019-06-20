9566986cb4cd52c1ae1a18620f9c17232ac8a1ba
Jared Burnham <zlolris97@gmail.com>
2019-06-20 20:32:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var recipes={
   
 }
 function updateObjectWithKeyAndValue(object, key, value){
- recipes= object.key = value
+  object.key = value
  return object
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
