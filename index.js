0a554703a15f831d7d474cafabd256ff2aee0424
Jared Burnham <zlolris97@gmail.com>
2019-06-20 20:52:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function deleteFromObjectByKey(object, key){
   return dupeRecipe
 }
 function destructivelyDeleteFromObjectByKey(object, key){
-  object= delete object.key;
+  object = delete object.key
   return object
 }
