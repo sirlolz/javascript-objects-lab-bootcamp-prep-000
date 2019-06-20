341640a0644ae0aae28f07008a8d92f56218a99f
Jared Burnham <zlolris97@gmail.com>
2019-06-20 20:34:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var recipes={
   
 }
 function updateObjectWithKeyAndValue(object, key, value){
-  object.key = value
- return object
+  Object.assign(object, { [key]: value })
+  return object
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key]=value
