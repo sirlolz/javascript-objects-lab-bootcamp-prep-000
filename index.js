fe123912b6df86ca4998941dc97248be6119514d
Jared Burnham <zlolris97@gmail.com>
2019-06-20 20:23:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ function updateObjectWithKeyAndValue(object, key, value){
   return object
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  
+  object[key]=value
+  return object
 }
