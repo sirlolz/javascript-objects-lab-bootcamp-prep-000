5b0b4ecfb06835eabd0b48c03a6a08e2ab7fb9fd
Jared Burnham <zlolris97@gmail.com>
2019-06-20 20:12:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var recipes={
   
 }
 function updateObjectWithKeyAndValue(object, key, value){
- var newob =  Object.assign({},object, key, value )
-   return recipes
+ return  Object.assign({},object, key, value )
+   return 
 }
