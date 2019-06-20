727472fdd8d03e48dba9ce94d74ccd14108f8f67
Jared Burnham <zlolris97@gmail.com>
2019-06-20 20:40:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object
 }
 function deleteFromObjectByKey(object, key){
-  return delete value[key];
+  return delete object[key];
 }
