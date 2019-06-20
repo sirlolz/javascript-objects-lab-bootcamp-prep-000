4b75685f1ec3d3810eeef6de114ec067b8606680
Jared Burnham <zlolris97@gmail.com>
2019-06-20 20:36:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var recipes={
   
-}
-function updateObjectWithKeyAndValue(object, key, value){
-  Object.assign(object, { [key]: value })
-  return object
+function updateObjectWithKeyAndValue(object, key, value) {
+ 
+  return Object.assign({}, object, { [key]: value })
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key]=value
